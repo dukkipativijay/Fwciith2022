87ee7b3f4bec183934f23a3faf694e2605fb16c4
dukkipativijay <111568872+dukkipativijay@users.noreply.github.com>
2022-09-05 10:49:49 +0530

    Update main.cpp

--- Assignment 1/Codes/src/main.cpp
+++ Assignment 1/Codes/src/main.cpp
@@ -6,33 +6,33 @@ int Z,Y,X,W,F;
 // Function for Displaying
 void disp_7447(int D, int C, int B, int A)
 {
-  digitalWrite(2, A); //LSB
-  digitalWrite(3, B); 
-  digitalWrite(4, C); 
-  digitalWrite(5, D); //MSB
+  digitalWrite(6, A); //LSB
+  digitalWrite(7, B); 
+  digitalWrite(8, C); 
+  digitalWrite(9, D); //MSB
 
 }
 
 
 // Initialising Input and Output Pins of Arduino
 void setup() {
-    pinMode(2, OUTPUT);  
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, INPUT);  
-    pinMode(7, INPUT);
-    pinMode(8, INPUT);
-    pinMode(9, INPUT);
+    pinMode(6, OUTPUT);  
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(2, INPUT);  
+    pinMode(3, INPUT);
+    pinMode(4, INPUT);
+    pinMode(5, INPUT);
     
 }
 
 // The Main Loop Function
 void loop() {
-X = digitalRead(6);//LSB  
-Y = digitalRead(7);  
-Z = digitalRead(8);  
-W = digitalRead(9);//MSB
+X = digitalRead(2);//LSB  
+Y = digitalRead(3);  
+Z = digitalRead(4);  
+W = digitalRead(5);//MSB
 
 F=(X&&!Y)||(X&&W)||(X&&Z)|(Y&&Z)||(Z&&!W);
 
