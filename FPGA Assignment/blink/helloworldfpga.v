34a82efbf0f9635ae3f614a54d4ec0d3765663fe
dukkipativijay <111568872+dukkipativijay@users.noreply.github.com>
2022-11-01 20:11:00 +0530

    Update helloworldfpga.v

--- FPGA Assignment/blink/helloworldfpga.v
+++ FPGA Assignment/blink/helloworldfpga.v
@@ -1,39 +1,10 @@
-/*Program for blinking LED
-January 16,2021
-Code by G V V Sharma
-Released under GNU GPL
-*/
+// Code for Boolean expression F
 
-//declaring the blink module
-module helloworldfpga(
-    output wire redled,
-    output wire greenled,
-    output wire blueled
+module boolean(
+	input X=1,Y=1,Z,=1,W=1,
+	output F
 );
-    wire clk;
 
-    qlal4s3b_cell_macro u_qlal4s3b_cell_macro (
-        .Sys_Clk0 (clk),
-    );
+assign F=(X&&!Y)||(X&&W)||(Y&&Z)||(Z&&!W);
 
-    reg[26:0] delay;
-     reg	led;
-always@(posedge clk) begin
-delay = delay+1; //incrementing the counter.
-//counts from 0 to 20000000 in 1 second
-
-//if(delay==27'b1001100010010110100000000) // blink delay in bits
-if(delay > 20000000) //blink delay in decimal
-begin
-delay=27'b0;
-led=!led; //reset the led
-end
-end
-//    assign blueled = led;	//If you want to change led colour to blue, 
-assign redled = led; //If you want to change led colour to red, 
 endmodule
-//end of the module
-
-
-
-
